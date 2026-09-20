8362bcc835c09c0a1ddf96f2372956f1d68435bc
Kowshik Naidu Valisetty <kowshikvkk710@gmail.com>
2026-09-20 20:14:56 +0530

    Time: 3 ms (97.65%) | Memory: 19.2 MB (87.39%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.py
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.py
@@ -1,6 +1,6 @@
 class Solution:
     def reverseDegree(self, s: str) -> int:
         total_sum = 0
-        for i, char_code in enumerate(s.encode('ascii'), 1):
-            total_sum += (123 - char_code) * i
+        for i, code in enumerate(s.encode('ascii'), 1):
+            total_sum += (123 - code) * i
         return total_sum
