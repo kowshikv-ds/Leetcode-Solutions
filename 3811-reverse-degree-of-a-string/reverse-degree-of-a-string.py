8e804fc4d7e9c0f26f33088e6f3c61c67adfff9e
Kowshik Naidu Valisetty <kowshikvkk710@gmail.com>
2026-09-20 20:08:15 +0530

    Time: 11 ms (20.35%) | Memory: 19.3 MB (55.57%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.py
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.py
@@ -1,34 +1,7 @@
 class Solution:
     def reverseDegree(self, s: str) -> int:
         ans = 0
-        hashed = {
-            'z' : 1,
-            'y' : 2,
-            'x' : 3,
-            'w' : 4,
-            'v' : 5,
-            'u' : 6,
-            't' : 7,
-            's' : 8,
-            'r' : 9,
-            'q' : 10,
-            'p' : 11,
-            'o' : 12,
-            'n' : 13,
-            'm' : 14,
-            'l' : 15,
-            'k' : 16,
-            'j' : 17,
-            'i' : 18,
-            'h' : 19,
-            'g' : 20,
-            'f' : 21,
-            'e' : 22,
-            'd' : 23,
-            'c' : 24,
-            'b' : 25,
-            'a' : 26,
-        }
-        for i, c in enumerate(s):
-            ans += (i+1) * hashed[c]
+        for i, char in enumerate(s):
+            pos = 26 - (ord(char) - ord('a'))
+            ans += pos * (i+1)
         return ans
