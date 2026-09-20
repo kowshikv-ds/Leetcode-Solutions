cd35ad9ca19978f0d9245fecd26606718c70bdc0
Kowshik Naidu Valisetty <kowshikvkk710@gmail.com>
2026-09-20 20:12:03 +0530

    Time: 7 ms (70.00%) | Memory: 19.3 MB (19.13%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.py
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.py
@@ -1,3 +1,7 @@
 class Solution:
+    _LOOKUP = [0] * 123
+    for i in range(26):
+        _LOOKUP[97 + i] = 26 - i
     def reverseDegree(self, s: str) -> int:
-        return sum((123 - ord(char)) * i for i, char in enumerate(s, 1))
+        lookup = self._LOOKUP
+        return sum(lookup[code] * i for i, code in enumerate(s.encode('ascii'), 1))
